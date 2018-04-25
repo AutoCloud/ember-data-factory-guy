d815a516ad01a7ac94bf908caa7a40b780dcf0a5
Oleg Bisyarin <bisyarin@mail.ru>
2018-04-25 14:44:37 +0300

    Revert "Allow passing "owner" into manualSetup()"
    This reverts commit 1d2a6f226e52cd1965353a2ea8777f0e3f4c1daa.

--- addon/utils/manual-setup.js
+++ addon/utils/manual-setup.js
@@ -3,12 +3,10 @@ import FactoryGuy from '../factory-guy';
 import loadFactories from './load-factories';
 import loadScenarios from './load-scenarios';
 
-export default function(ownerOrContainer) {
-  let owner = ownerOrContainer.owner || ownerOrContainer;
-
-  FactoryGuy.setStore(owner.lookup('service:store'));
+export default function(container) {
+  FactoryGuy.setStore(container.lookup('service:store'));
   FactoryGuy.resetDefinitions();
   loadFactories();
-  loadScenarios(owner);
+  loadScenarios(container);
 }
 
