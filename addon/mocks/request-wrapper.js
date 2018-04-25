f2cd453c9bf44685d83edce5ebae143c38ff1c26
Oleg Bisyarin <bisyarin@mail.ru>
2018-04-25 14:44:37 +0300

    Revert "sort function as static method"
    This reverts commit 2c57aea39ed6230ca5d0b29674ed05dba82f1a96.

--- addon/mocks/request-wrapper.js
+++ addon/mocks/request-wrapper.js
@@ -1,4 +1,4 @@
-import { isEmptyObject } from '../utils/helper-functions';
+import {isEmptyObject} from '../utils/helper-functions';
 
 /**
  * This request wrapper controls what will be returned by one url / http verb
@@ -26,7 +26,7 @@ import { isEmptyObject } from '../utils/helper-functions';
  *  and both of these hanlders will reside in the list for the wrapper that
  *  belongs to [GET /users]
  */
-export default class RequestWrapper {
+export default class {
 
   constructor() {
     this.index = 0;
@@ -58,7 +58,12 @@ export default class RequestWrapper {
   getHandlers(request) {
     if (request && !isEmptyObject(request.queryParams)) {
       // reverse sort so query comes before findAll
-      return this.handlers.sort(RequestWrapper.reverseSortByRequestType);
+      return this.handlers.sort((a, b) => {
+        if (b.requestType < a.requestType) {
+          return -1;
+        }
+        return (b.requestType > a.requestType) ? 1 : 0;
+      });
     }
     return this.handlers;
   }
@@ -100,15 +105,8 @@ export default class RequestWrapper {
   handleRequest(request) {
     let handler = this.getHandlers(request).find(handler => handler.matches(request));
     if (handler) {
-      let {status, headers, responseText} = handler.getResponse();
+      let { status, headers, responseText } = handler.getResponse();
       return [status, headers, responseText]
     }
-  }
-
-  static reverseSortByRequestType(a, b) {
-    if (b.requestType < a.requestType) {
-      return -1;
-    }
-    return (b.requestType > a.requestType) ? 1 : 0;
   }
 }
