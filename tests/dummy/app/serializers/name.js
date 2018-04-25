debbecf16617ddbecbdc266d114387e9315e2b4b
Oleg Bisyarin <bisyarin@mail.ru>
2018-04-25 14:44:37 +0300

    Revert "small clean up in tests"
    This reverts commit d9167447f1697f69c850779f1f7cbf93d3c2b0ca.

--- tests/dummy/app/serializers/name.js
+++ tests/dummy/app/serializers/name.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   keyForAttribute(attr) {
+//    console.log('name here',Ember.String.decamelize(attr));
     return Ember.String.decamelize(attr);
+//    return this._super(...arguments);
   }
 });
