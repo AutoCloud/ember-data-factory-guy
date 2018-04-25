99734784025d26144fbea1e37cfe679924f54741
Oleg Bisyarin <bisyarin@mail.ru>
2018-04-25 14:44:37 +0300

    Revert "clean up test-helper"
    This reverts commit ceef2bbe96926f27e3e9fb8d9cb7d6d2d3e65ab5.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,9 @@
 import Application from '../app';
 import { setApplication } from '@ember/test-helpers';
+//import resolver from './helpers/resolver';
+//import { setResolver } from '@ember/test-helpers';
 import { start } from 'ember-cli-qunit';
 
 setApplication(Application.create({autoboot: false, rootElement: '#ember-testing'}));
+//setResolver(resolver);
 start();
